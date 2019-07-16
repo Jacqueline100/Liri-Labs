7f79a2940c383be05edb8520dff757e89cc8a8aa
Jacqueline100 <Jacqueline100@yahoo.com>
2019-07-16 01:58:32 -0400

    auth

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const postRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
 const app = express();
 
 //connect the server and replace the password
@@ -28,4 +29,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
+
 module.exports = app;
